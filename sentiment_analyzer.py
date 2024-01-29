f16ce5678e8af642da32f2bd71bf76b983bd6982
AJurBoii <lesurea2@carleton.edu>
2024-01-29 02:10:01 -0600

    Added requirements.txt

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -10,11 +10,6 @@ Doing so will ensure that the MissingCorpusError won't occur while running.
 from nrclex import NRCLex
 import json
 
-# text = open("sample.txt", 'r')
-# text_object = NRCLex(text.read())
-# print(text_object.words)
-# file.close()
-
 file = open('all_articles1.json')
 data = json.load(file)
 
