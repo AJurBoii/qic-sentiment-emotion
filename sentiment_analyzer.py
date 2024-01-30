ee536f726768eb1914effa0b2b6475ef27e207b5
AJurBoii <lesurea2@carleton.edu>
2024-01-29 19:59:31 -0600

    Collected affect dict from all_articles1.json

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -10,16 +10,28 @@ Doing so will ensure that the MissingCorpusError won't occur while running.
 from nrclex import NRCLex
 import json
 
-# file = open('all_articles1.json')
-# data = json.load(file)
+file = open('all_articles1.json')
+data = json.load(file)
 
-# text_object = NRCLex(data[0]['content'])
-# print(text_object.affect_dict)
-# file.close()
+total = len(data)
+current = 1
 
-file = open('sample.txt', 'r')
-text = file.read()
-text_object = NRCLex(text)
+all_articles_text = ""
+for article in data:
+    all_articles_text += str(article['content'])
+    print("\r" + str(current) + "/" + str(total), end='\r')
+    current += 1
+
+text_object = NRCLex(all_articles_text)
+
+print("Printing affect dict to output...")
 
 print(text_object.affect_dict)
 file.close()
+
+# file = open('sample.txt', 'r')
+# text = file.read()
+# text_object = NRCLex(text)
+
+# print(text_object.affect_dict)
+# file.close()
