4d9272766e7d249027e264203ed3b19288554bb4
AJurBoii <lesurea2@carleton.edu>
2024-02-13 19:22:24 -0600

    Consistent label-to-color matching across all pie charts

--- graphs.py
+++ graphs.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-article_affects = {'fear': 0.0674939280121633, 'anger': 0.06059309720816781, 'trust': 0.1541022584325241, 'surprise': 0.05005209066861874, 'positive': 0.23911783626456767, 'negative': 0.10756575649816863, 'sadness': 0.05715567533217143, 'disgust': 0.027095992878862076, 'joy': 0.09212697918142294, 'anticipation': 0.14469638552333328}
-quotes_affects = {'fear': 0.05547474577153782, 'anger': 0.04809325553772946, 'trust': 0.15820065017022955, 'surprise': 0.061133359287242825, 'positive': 0.2512255783217078, 'negative': 0.08418291059043491, 'sadness': 0.04401766518960819, 'disgust': 0.02355540457102899, 'joy': 0.11576402440989984, 'anticipation': 0.1583524061505806}
-
+# Determines the order in which data is plotted on the pie charts;
+# Ensures that the chart for each corpus uses the same colors for the same emotions
+label_order = ['anticipation', 'joy', 'positive', 'fear', 'trust', 'negative', 'sadness', 'disgust', 'surprise', 'anger']
 
 '''
 Input: text file where each line is a dictionary, where keys are emotions and values are
@@ -60,7 +60,7 @@ def pie_chart(aggregate_file, chart_name):
         print(frequencies)
         chart_labels = []
         data = []
-        for key in frequencies.keys():
+        for key in label_order:
             chart_labels.append(key)
             data.append(frequencies[key])
         fig, ax = plt.subplots()
