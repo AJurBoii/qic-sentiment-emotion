f63341a89d89e67de6ecb2f35679b56c2eec14ab
AJurBoii <lesurea2@carleton.edu>
2024-02-20 22:10:43 -0600

    Compute frequencies of pairs and their average differences, take 5 most and 5 least different pairs

--- aggregate.py
+++ aggregate.py
@@ -1,11 +1,13 @@
+from nrclex import NRCLex
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import sys
+import csv
 
 # Determines the order in which data is plotted on the pie charts;
 # Ensures that the chart for each corpus uses the same colors for the same emotions
-label_order = ['anticipation', 'joy', 'positive', 'fear', 'trust', 'negative', 'sadness', 'disgust', 'surprise', 'anger']
+emotions = ['positive', 'negative', 'anticipation', 'joy', 'fear', 'trust', 'sadness', 'disgust', 'surprise', 'anger']
 
 '''
 Input: text file where each line is a dictionary, where keys are emotions and values are
@@ -40,16 +42,86 @@ def aggregate(text_file, output_name):
         file.write('\nAggregated frequencies:\n')
         file.write(str(aggregated_frequencies))
 
+    
+'''
+    input: raw_matched_trans.csv, a csv file where each row contains
+    an article and a transcript that have been matched to each other based on the
+    presence of the same quote in both texts
+
+    output: MATCHED_art_trans.AFFECT_FREQUENCIES.csv, a csv file with 4 columns:
+    article emotions, transcript emotions, emotion differences, average emotion difference
+
+'''
+def matched_analysis(text_file):
+    with open(text_file, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        output = open('outputs/matched articles transcripts/MATCHED_art_trans.AFFECT_FREQUENCIES.csv', 'w', newline='')
+        fieldnames = ['article_frequency', 'transcript_frequency', 'emotion_differences', 'average_difference']
+        writer = csv.DictWriter(output, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for row in reader:
+            article_object = NRCLex(row['raw_article'])
+            transcript_object = NRCLex(row['raw_transcript'])
+
+            article_freq = article_object.affect_frequencies
+            transcript_freq = transcript_object.affect_frequencies
+
+            # delete faulty 'anticip' value from frequency dict
+            del article_freq['anticip']
+            del transcript_freq['anticip']
+
+            differences = {}
+            total = 0
+            for emotion in emotions:
+                diff = abs(article_freq[emotion] - transcript_freq[emotion])
+                total += diff
+                differences[emotion] = diff
+            average = total / len(emotions)
+
+            row_to_write = {'article_frequency': article_freq, 'transcript_frequency': transcript_freq, 'emotion_differences': differences, 'average_difference': average}
+            writer.writerow(row_to_write)
+
+'''
+    input: csv file with 4 columns, where one column contains average difference of
+    emotion distributions between an article and a transcript document
+
+    output: top_bottom_five_matches.txt, the values of the top and bottom 5 average
+    differences
+'''
+def top_bottom_five(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+
+        # first loop: collect averages to sort
+        all_averages = []
+        for row in reader:
+            all_averages.append(float(row['average_difference']))
+
+        # sort in both ascending (bottom 5) and descending (top 5) order
+        all_averages.sort()
+        bottom_five = all_averages[:5]
+        all_averages.sort(reverse=True)
+        top_five = all_averages[:5]
+
+        outfile = open('outputs/matched articles transcripts/top_bottom_five_matches.txt', 'w')
+        outfile.write(f'Top 5:\n{str(top_five)}\n')
+        outfile.write(f'Bottom 5:\n{bottom_five}')
+
+        outfile.close()
+
+
+
 ''' Taking in command line arguments:
     FORMAT:
         python graphs.py [a/aggregate/g/graph] [file_name] [label for generated file/chart]
-'''        
+'''    
 num_args = len(sys.argv)
-if num_args < 4:
-    print(f'3 arguments required. Only {num_args-1} provided.')
+if num_args < 3:
+    # matched_analysis('data/raw_matched_art_trans.csv')
+    top_bottom_five('outputs/matched articles transcripts/MATCHED_art_trans.AFFECT_FREQUENCIES.csv')
 else:
-    file_name = sys.argv[2]
-    label = sys.argv[3]
-    if str(sys.argv[1]) == 'aggregate' or str(sys.argv[1]) == 'a':
-        aggregate(file_name, label)
-        print("\nFile aggregated successfully")
+    file_name = sys.argv[1]
+    label = sys.argv[2]
+    aggregate(file_name, label)
+    print("\nFile aggregated successfully")
